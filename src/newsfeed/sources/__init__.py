69fef0c27e2c5ffe7b9d2d5f4adf113e66268d5b
SnailShepherd <dm.3635287@gmail.com>
2025-09-20 01:55:37 +0200

    Delete src directory

--- src/newsfeed/sources/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import _common, notim, minfin, eec, gostinfo, erzrf, interfax_realty, metalinfo, pnp, rg, government, stroygaz, ria_realty
-__all__ = ['_common','notim','minfin','eec','gostinfo','erzrf','interfax_realty','metalinfo','pnp','rg','government','stroygaz','ria_realty']
