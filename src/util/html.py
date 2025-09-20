8f3e8be906f826ce5940a6dfbe66db55bb62afe7
SnailShepherd <dm.3635287@gmail.com>
2025-09-20 02:21:44 +0200

    Delete src directory

--- src/util/html.py
+++
@@ -1,3 +0,0 @@
-from bs4 import BeautifulSoup
-def soup_html(text):
-    return BeautifulSoup(text, "lxml")
--- src/util/rss.py
+++
@@ -1,5 +0,0 @@
-import feedparser, chardet
-def parse_rss_bytes(content: bytes):
-    enc = chardet.detect(content).get("encoding") or "utf-8"
-    text = content.decode(enc, errors="replace")
-    return feedparser.parse(text)
